e51a3eb38aaa8f5278bae9804a77fbc60cae227c
gennadiy-gaysha <gennadiy-gaysha@gmail.com>
2023-07-30 15:00:12 +0200

    Prepare settings.py for deployment-5

--- codestar/settings.py
+++ codestar/settings.py
@@ -93,13 +93,13 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+# }
 # ==================================================
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
